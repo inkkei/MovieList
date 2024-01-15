1cf65318a8295b20edeb22f542302dd8ba363b88
inkkei <51513928+inkkei@users.noreply.github.com>
2024-01-15 07:15:26 -0700

    cors fixed

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ import cors from "cors";
 const app = express();
 
 // middleware
-app.use(cors());
+const corsOptions = {
+  origin: "https://movielist-3l4h.onrender.com", // frontend URI (ReactJS)
+};
+app.use(cors(corsOptions));
+/* app.use(cors());
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
@@ -20,7 +24,7 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   next();
-});
+}); */
 
 app.use(express.json());
 
